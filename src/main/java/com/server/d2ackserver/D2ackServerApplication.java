66f462b973d27afab4302aebf6227561889620a1
Simseoh <seohoon0707@gmail.com>
2024-12-06 13:48:49 +0900

    make repo

---
+++ src/main/java/com/server/d2ackserver/D2ackServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.d2ackserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2ackServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D2ackServerApplication.class, args);
+    }
+
+}
