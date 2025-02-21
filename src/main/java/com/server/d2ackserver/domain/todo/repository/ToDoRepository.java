347f4d8b2983bee6ba08cc2a7ca6f36d5ec5105d
Simseoh <seohoon0707@gmail.com>
2025-02-22 05:15:13 +0900

    DeleteTodo

--- src/main/java/com/server/d2ackserver/domain/todo/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.server.d2ackserver.domain.todo.repository;
-
-import com.server.d2ackserver.domain.todo.domain.entity.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-}
